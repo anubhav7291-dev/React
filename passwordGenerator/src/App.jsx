9edbad262d20ec749481c2ecf29493852cf55505
anubhav7291-dev <anubhavgupta1511@gmail.com>
2025-09-04 16:12:48 +0000

    password generator begins

---
+++ passwordGenerator/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+  const [lenght,setLenght] = useState(8)
+  const [numberAllowed,setNumberAllowed] = useState(false)
+  const [charAllowed,setCharAllowed] = useState(false)
+  const [password,setPassword]=useState("")
+  return (
+    <>
+      <h1 className='text-4xl text-centre text-white'>Password generator</h1>
+    </>
+  )
+}
+
+export default App
