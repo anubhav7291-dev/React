0612bc17301c0e854ec0924cf292884abb1d703c
anubhav7291-dev <anubhavgupta1511@gmail.com>
2025-09-08 09:55:51 +0000

    ........

---
+++ Router/src/components/Github/Github.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Github(){
+    return(
+        <div className="bg-gray-600 text-white text-3xl p-4 text-centre">
+            Github followers : 
+        </div>
+    )
+}
+
+export default Github
