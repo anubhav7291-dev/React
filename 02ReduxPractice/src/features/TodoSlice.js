1544a0e7b2f4900961c018dff76726cfed19ba51
anubhav7291-dev <anubhavgupta1511@gmail.com>
2025-09-16 19:31:11 +0000

    mistakes in practice

---
+++ 02ReduxPractice/src/features/TodoSlice.js
@@ -0,0 +1,27 @@
+import { nanoid , createSlice} from "@reduxjs/toolkit";
+import React from "react";
+import { useState } from "react";
+
+const initialState = {
+    todos : [{id:1,text:"Hello World"}]
+}
+
+export const TodoSlice = createSlice({
+    name: 'todo',
+    initialState,
+    reducer:{
+        addTodo:(state,action)=>{
+            const todo={
+                id:nanoid(),
+                text:action.payload
+            }
+            state.todos.push(todo)
+        },
+        removeTodo:(state,action)=>{
+            state.todos = state.todos.filter((todo)=> todo.id!=action.payload)
+        }
+    }
+})
+
+export const {addTodo,removeTodo} = TodoSlice.actions
+export default TodoSlice.reducer
